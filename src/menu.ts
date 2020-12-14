52acdf30708a3690dce21df1428ad603dca20edb
Olof-Wallgren <naturel.olof@hotmail.com>
2020-12-14 13:58:04 +0100

    menu class created

--- src/menu.ts
+++ src/menu.ts
@@ -1,3 +1,19 @@
 class Menu {
-    
+     private menuAudio:MenuAudio
+     private gameOver:GameOver
+     private pauseScreen:PauseScreen
+     private titleScreen:TitleScreen
+     private controls:Controls
+     constructor(){}
+
+     public startGame(){
+
+     }
+     public quit(){
+
+     }
+     public draw(){
+         
+     }
+
 }
