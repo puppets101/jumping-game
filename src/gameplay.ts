3e9d2714e6a840a092982243cf713fd2161db7b1
puppets101 <oscand92@gmail.com>
2020-12-14 14:01:10 +0100

    Added properties to class GamePlay

--- src/gameplay.ts
+++ src/gameplay.ts
@@ -1,3 +1,21 @@
 class GamePlay {
-    
+  private score: Score;
+  private character: Character;
+  private obstacle: Obstacle;
+  private platform: Platform;
+  private background: Background;
+  private lives: Lives;
+  private gameAudio: GameAudio;
+  private pauseScreen: PauseScreen;
+  private projectile: Projectile;
+  private drawableEntity: DrawableEntity;
+  private MovableEntity: MovableEntity;
+
+  constructor() {}
+
+  draw() {}
+
+  pauseGame() {}
+
+  gameOver() {}
 }
