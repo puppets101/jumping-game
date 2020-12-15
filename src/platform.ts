976e8aea250b4ea3fdd1e7333000b068ae87ac91
puppets101 <oscand92@gmail.com>
2020-12-15 10:46:25 +0100

    Updated classes Platform and GamePlay

--- src/platform.ts
+++ src/platform.ts
@@ -1,3 +1,12 @@
 class Platform extends MovableEntity {
   private color: number;
+
+  constructor(_color: number) {
+    super();
+    this.color = _color;
+  }
+
+  public update() {}
+
+  public draw() {}
 }
