99c55ee24525d25de837b4356d6b6307a8ce2e81
Lisa Pääjärvi <65873813+lisapaajarvi@users.noreply.github.com>
2020-12-16 18:46:12 +0100

    obstacle lands on the ground

--- src/obstacle.ts
+++ src/obstacle.ts
@@ -4,7 +4,7 @@ class Obstacle extends MovableEntity {
     protected width: number;
 
     constructor() {
-        super(createVector(500, 0), true, createVector(0, 5), 0); 
+        super(createVector(500, 0), true, createVector(0, 1), 0); 
         this.color = color(146, 211, 202);
         this.height = 30;
         this.width = 30;
@@ -15,8 +15,10 @@ class Obstacle extends MovableEntity {
         this.position.y += this.velocity.y;
         this.position.x -= this.velocity.x;
 
-        if ((this.position.y + this.height === 250 && (platform1.position.x > 400 && platform1.position.x < 550 || platform2.position.x > 400 && platform2.position.x < 550)) 
-            || (this.position.y + this.height === 150 && (platform3.position.x > 400 && platform3.position.x < 550 || platform4.position.x > 400 && platform4.position.x < 550))) {
+        if ((this.position.y + this.height === 250 && (platform1.position.x > 400 && platform1.position.x < 500 || platform2.position.x > 400 && platform2.position.x < 500)) 
+            || (this.position.y + this.height === 150 && (platform3.position.x > 400 && platform3.position.x < 500 || platform4.position.x > 400 && platform4.position.x < 500))
+            || (this.position.y + this.height === 400))
+            {
         this.velocity.y = 0;
         this.velocity.x = 3;
         }
