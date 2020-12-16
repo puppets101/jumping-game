d86eb7c42810f82c323af31bb52f4cfc60e54527
mariahelenanoren <marialinder97@hotmail.com>
2020-12-16 15:21:30 +0100

    platform position resets when at the end of the screen

--- src/platform.ts
+++ src/platform.ts
@@ -4,18 +4,21 @@ class Platform extends MovableEntity {
   protected width: number;
 
   constructor() {
-    super(createVector(300, 400 - 200), true, createVector(0, 0), 0);
+    super(createVector(600, 400 - 200), true, createVector(2, 0), 0); // Changing velocity.x alters speed
     this.color = color(100, 50, 150);
     this.height = 20;
     this.width = 100;
   }
 
   public update() {
-    this.velocity = createVector(this.position.x -= 1, 0)
+    this.position.x -= this.velocity.x;
+    if (this.position.x === 0 - this.width) {
+      this.position.x = 600
+    }
   }
 
   public draw() {
     fill(this.color)
-    rect(this.position.x + this.velocity.x, this.position.y, this.width, this.height)
+    rect(this.position.x, this.position.y, this.width, this.height)
   }
 }
