3692fbb1786bba89602f5351446b6f6661e9a856
Lisa Pääjärvi <65873813+lisapaajarvi@users.noreply.github.com>
2021-01-06 14:54:48 +0100

    added functions

--- src/powerup.ts
+++ src/powerup.ts
@@ -6,9 +6,31 @@ class Powerup extends MovableEntity {
 
     constructor() {
         super(createVector(500, 0), true, createVector(0, 10), 0);
-        this.color = color(0, 255, 255);
+        this.color = color(220, 20, 60);
         this.height = 20;
         this.width = 20;
         //this.powerupAsset = powerupAsset;
     }
+
+public update() {
+    this.position.y += this.velocity.y;
+    this.position.x -= this.velocity.x;
+    this.isOnScreen()
+}
+
+public draw() {
+    fill(this.color)
+    rect(this.position.x, this.position.y, this.width, this.height)
+
+    //image(this.powerupAsset, this.position.x, this.position.y, this.width, this.height)
+}
+
+private isOnScreen() {
+    if (this.position.x + this.width < 0) {
+        this.isVisible = false;
+    }
+    else {
+        this.isVisible = true;
+    }
+}
 }
