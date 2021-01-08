0a59deab6d8ee0b73da259bb6389ac22495bc3d3
Lisa Pääjärvi <65873813+lisapaajarvi@users.noreply.github.com>
2021-01-08 19:47:05 +0100

    added interval to backstory text

--- src/titleScreen.ts
+++ src/titleScreen.ts
@@ -1,5 +1,6 @@
 class TitleScreen {
   private backstory: string[];
+  private backstoryTimer: number;
   private menu: Imenu;
   private fade: number;
   private fadeAmount: number;
@@ -7,6 +8,7 @@ class TitleScreen {
 
   constructor(menu: Imenu) {
     this.backstory = ["Neo-Tokyo, Earth, 2076.", "The latest Intergalactic War and subsequent waves of Alien Invasion have left humanity at the verge of extinction, its cities deserted and crawling with Alien life forms on the lookout for any remaining signs of human activity.", "Your only chance of survival is to keep running, avoiding all contact while shooting as many Aliens as you can.", "Do you have what it takes to stay alive?"];
+    this.backstoryTimer = 15000;
     this.menu = menu;
 
     //fade in and out for the press any key text
@@ -42,13 +44,7 @@ class TitleScreen {
       text("Press any key", 400, 500);
 
       // text for backstory
-      textSize(13);
-      fill(212,235,242);
-      textAlign(LEFT);
-      text(this.backstory[0], 50, 250);
-      text(this.backstory[1], 50, 275, 700, 300);
-      text(this.backstory[2], 50, 375, 700, 300);     
-      text(this.backstory[3], 50, 450);      
+      this.drawBackstoryInIntervals();    
 
       // making the text fade in and out
       if (this.fade <= 40) {
@@ -59,4 +55,24 @@ class TitleScreen {
       this.fade += this.fadeAmount;
     
   }
+  private drawBackstoryInIntervals() {
+    textSize(13);
+    fill(212,235,242);
+    textAlign(LEFT);this.backstoryTimer -= deltaTime;
+
+    if (this.backstoryTimer < 14000) {
+      text(this.backstory[0], 50, 250);
+    }
+    if (this.backstoryTimer < 11000) {
+      text(this.backstory[1], 50, 275, 700, 300);
+    }
+    if (this.backstoryTimer < 5000) {
+      text(this.backstory[2], 50, 375, 700, 300);   
+    }
+    if (this.backstoryTimer < 0) {  
+      text(this.backstory[3], 50, 450);  
+    }
+  }
 }
+
+
